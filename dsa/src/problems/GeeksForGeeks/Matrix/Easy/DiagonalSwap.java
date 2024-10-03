e579beb7dedea5002f4b46e3acf782a67b2cff4e
gangadhar v <gangadharv778@gmail.com>
2024-10-03 09:50:07 +0530

    3/10/2024

--- dsa/src/problems/GeeksForGeeks/Matrix/Easy/DiagonalSwap.java
+++ dsa/src/problems/GeeksForGeeks/Matrix/Easy/DiagonalSwap.java
@@ -1,2 +1,45 @@
-package problems.GeeksForGeeks.Matrix.Easy;public class DiagonalSwap {
+package problems.GeeksForGeeks.Matrix.Easy;
+
+
+import java.util.Arrays;
+
+/*
+PROBLEM STATEMENT
+
+major diagonal -> 0,4,8
+minor diagonal -> 2,4,6
+Input : 0 1 2
+        3 4 5
+        6 7 8
+
+Output : 2 1 0
+         3 4 5
+         8 7 6
+
+
+ */
+public class DiagonalSwap {
+
+    public static void swapDiagonal(int[][] matrix){
+
+        int N = matrix.length;
+        for(int i = 0;i<N;i++){
+
+            int m = matrix[i][i];
+            matrix[i][i] = matrix[i][N-1-i];
+            matrix[i][N-1-i] = m;
+        }
+
+        System.out.println(Arrays.deepToString(matrix));
+
+    }
+    public static void main(String args[]){
+        int[] a = {0,1,2};
+        int[] b = {3,4,5};
+        int[] c = {6,7,8};
+
+        int[][] matrix = {a,b,c};
+        swapDiagonal(matrix);
+    }
+
 }
--- dsa/src/problems/GeeksForGeeks/Matrix/Easy/RowColTraverse.java
+++ dsa/src/problems/GeeksForGeeks/Matrix/Easy/RowColTraverse.java
@@ -1,2 +1,60 @@
-package problems.GeeksForGeeks.Matrix.Easy;public class RowColTraverse {
+package problems.GeeksForGeeks.Matrix.Easy;
+
+import java.util.Arrays;
+
+/* PROBLEM STATEMENT
+
+Input : mat[][] = {{1, 2, 3},
+                   {4, 5, 6},
+                   {7, 8, 9}}
+
+
+Output : Row-wise: 1 2 3 4 5 6 7 8 9
+         Col-wise : 1 4 7 2 5 8 3 6 9
+*/
+public class RowColTraverse {
+
+
+    public static void RC(int size,int[][] arr){
+
+
+        int colIndex = 0;
+        int[] rowElements = new int[size*size];
+        int[] colElements = new int[size*size];
+
+        int rowindex =0;
+        int rowLength = arr.length;
+        int colLength = arr[0].length;
+
+        for(int row = 0;row<rowLength;row++){
+
+            for(int col = 0;col<colLength;col++){
+                rowElements[rowindex++] = arr[row][col];
+            }
+        }
+
+        int colindex = 0;
+
+        for(int col = 0;col<colLength;col++){
+            for(int row = 0;row<rowLength;row++){
+
+                colElements[colindex++] = arr[row][col];
+
+            }
+        }
+
+        System.out.println(Arrays.toString(rowElements));
+        System.out.println(Arrays.toString(colElements));
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {4, 5, 6};
+        int[] a3 = {7, 8, 9};
+        int[][] matrix = {a1,a2,a3};
+        RC(3,matrix);
+
+    }
 }
